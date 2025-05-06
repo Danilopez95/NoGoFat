6a6e04f8c6e75c854e6a5589038d765e045ed95b
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-05-06 16:49:30 +0200

    Commit

--- DesktopApp/src/main/kotlin/BBDD/ConexionBBDD.kt
+++ DesktopApp/src/main/kotlin/BBDD/ConexionBBDD.kt
@@ -1,15 +1,7 @@
 package BBDD
 import java.sql.*
 
-<<<<<<< HEAD
-import java.sql.*
-=======
-class ConexionBBDD {
-    val user = "root"
-    val url = "jbcc:mysql://localhost/supermercado"
-    val pass = ""
-    var conn: Connection? = null
->>>>>>> 023474607f6e6b9a743418c5c48cd64a9cf5cab8
+
 
 class ConexionBD {
     val url = "jdbc:mysql://localhost/supermercado"
@@ -46,8 +38,6 @@ class ConexionBD {
     fun getPreparedStatement(sql: String): PreparedStatement? {
         return conn?.prepareStatement(sql)
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 023474607f6e6b9a743418c5c48cd64a9cf5cab8
+
+
