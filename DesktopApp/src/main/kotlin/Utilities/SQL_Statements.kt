e3ea9d6b2254dff2f3d672b5b7b3222aab429475
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-05-12 09:40:24 +0200

    Update SQL_Statements.kt

--- DesktopApp/src/main/kotlin/Utilities/SQL_Statements.kt
+++ DesktopApp/src/main/kotlin/Utilities/SQL_Statements.kt
@@ -2,14 +2,14 @@ package Utilities
 
 object SQL_Statements {
     const val SELECT_ALL_TABLES = "SHOW TABLES"
-<<<<<<< HEAD
+
 
     /* SELECT DANI */
 
     const val SELECT_ALL_TELEFONOS = "SELECT * FROM TELEFONO"
 
-=======
 
-    const val SELECT_ADRIAN = "SLECT"
->>>>>>> 1d038d03f21ad28e6453b9d4298afa9bf9de2784
+
+    const val SELECT_ADRIAN = "SELECT"
+
 }
