3ab4865f6968e8609369ad377fea9f6387317751
AdriAST <astadrian1@gmail.com>
2025-05-13 09:07:52 +0200

    CAmbios

--- DesktopApp/src/main/kotlin/BBDD/EmpleadoDAO.kt
+++ DesktopApp/src/main/kotlin/BBDD/EmpleadoDAO.kt
@@ -2,11 +2,6 @@ package BBDD
 
 import Negocio.Empleado
 
-import Negocio.Tabla
-
-interface EmpleadoDAO {
-    fun getAllEmpleados(): List<Empleado>
-
 
 interface EmpleadoDAO {
     fun getAllEmpleadosInfo():List<Empleado>
