069db6772b9d7f724a561d674a2b6d25cefee677
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-05-14 14:14:54 +0200

    Create UsuarioRutina.kt

---
+++ DesktopApp/src/main/kotlin/Negocio/UsuarioRutina.kt
@@ -0,0 +1,28 @@
+package Negocio
+
+class UsuarioRutina {
+    var id_usuario:Int
+    var nombre:String
+    var apellido1:String
+    var rutina:String
+
+    constructor(apellido1: String, id_usuario: Int, nombre: String, rutina: String) {
+        this.apellido1 = apellido1
+        this.id_usuario = id_usuario
+        this.nombre = nombre
+        this.rutina = rutina
+    }
+
+    override fun toString(): String {
+        return "UsuarioRutina(apellido1='$apellido1', id_usuario=$id_usuario, nombre='$nombre', rutina='$rutina')"
+    }
+
+
+    /**
+     * UsuarioRutina(
+     *                     preparedStatement.getInt("ID_USUARIO"),
+     *                     preparedStatement.getString("NOMBRE"),
+     *                     preparedStatement.getString("APELLIDO1"),
+     *                     preparedStatement.getString("RUTINA")
+     */
+}
